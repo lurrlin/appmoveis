955a4be0bdf38ab621fb2853fc3b106c7c574915
Murilo Branco <murilo.b08@aluno.ifsc.edu.br>
2026-08-03 21:58:08 -0300

    ft(1): Primeiro commit (conta cliques)

---
+++ app/src/main/java/com/ifsc/app/MainActivity.java
@@ -0,0 +1,36 @@
+package com.ifsc.app;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Integer contador = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_main); /// constraint, button e Textview
+        TextView tv = findViewById(R.id.text);
+        tv.setText("new Text");
+        Button btn = findViewById(R.id.button);
+        btn.setText("Clique aqui");
+        btn.setOnClickListener(v -> {
+            contador++;
+            tv.setText("Você clicou "  + Integer.toString(contador) + " Vezes");
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
