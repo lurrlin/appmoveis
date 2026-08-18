d825e9ba938b21586d858163418dfde7db55a63b
lucas 1708 <lucas.1708@ifsc.edu.br>
2026-08-17 21:54:28 -0300

    toast e min max

--- app/src/main/java/com/ifsc/app/MainActivity.java
+++ app/src/main/java/com/ifsc/app/MainActivity.java
@@ -2,7 +2,9 @@ package com.ifsc.app;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +12,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-    Integer contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +22,55 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main); /// constraint, button e Textview
 
+        EditText edMin = findViewById(R.id.edMin);
+        EditText edMax = findViewById(R.id.edMax);
+
         TextView tv = findViewById(R.id.text);
         tv.setText("Melão");
 
         Button btn = findViewById(R.id.button);
         btn.setText("Tem Risquinho?");
 
+        Random rnd = new Random();
+
         btn.setOnClickListener(v -> {
-            contador++;
-            tv.setText("Tem "  + Integer.toString(contador) + " Risquinho");
-            btn.setText("ta doce irmao");
+
+            String Smin = edMin.getText().toString();
+            String Smax = edMax.getText().toString();
+
+            if(Smin.isEmpty()){
+                edMin.setError("Informe um Inteiro");
+                return;
+            }
+
+            if(Smax.isEmpty()){
+                edMax.setError("Informe um Inteiro");
+                return;
+            }
+
+            int min = Integer.parseInt(Smin);
+            int max = Integer.parseInt(Smax);
+
+            if (max < min) {
+                Toast.makeText(this, "Mínimo deve ser Menor", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            int aleat = rnd.nextInt((max - min) + 1) + min;
+
+            if (aleat == 1) {
+                tv.setText("Tem " + aleat + " Risquinho");
+            } else {
+                tv.setText("Tem " + aleat + " Risquinhos");
+            }
+
+            if(aleat<=15){
+                btn.setText("Podia ta melhor mano...");
+            } else {
+                btn.setText("Ta uma doçura papai!");
+            }
+
+
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
